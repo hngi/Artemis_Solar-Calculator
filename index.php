436141d334f7a8fd5e22ce7cf712a0e256a4fd6e
AgbenigaAgboola <emmanuelagbeniga@gmail.com>
2019-09-23 23:44:10 +0100

    Index Markup for Solar Calculato

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<!-- The  content-container carries all the texts and links  -->
+    <div class="content-conatainer"> 
+​
+        <!-- Navigation bar -->
+        <div class="navbar">
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">PRODUCTS</a></li>
+                <li><a href="#">SERVICE</a></li>
+                <li><a href="#">CONTACT</a></li>
+            </ul>
+        </div>
+​
+        <!-- Page-text  -->
+        <div class="welcome">Welcome to</div>
+        <div class="artemis">Artemis</div>
+        <div class="solar">Solar Load Calculator</div>
+        
+        <!-- Explore button -->
+        <a href="#" class="explore-button">Explore</a>
+    </div>
+</body>
+</html>
