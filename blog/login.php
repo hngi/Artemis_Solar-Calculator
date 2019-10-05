dfe86fd641d7aef3047cd86aa26432ed4dab74c0
AgbenigaAgboola <emmanuelagbeniga@gmail.com>
2019-10-05 01:59:34 +0100

    Updating Master

---
+++ blog/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet"  type="text/css" href="../style/admin_login.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Page</title>
+</head>
+<body>
+    <div class="loginbox">
+        <img src="https://res.cloudinary.com/wpgroom-com/image/upload/v1569383582/HEADER_pz58tu.png" width="100" class="logo">
+        <h1>Login Here</h1>
+        <form>
+            <p>Username</p>
+            <input type="text" name="" placeholder="Enter Username">
+            <p>Password
+            </p>
+            <input type="password" name="" placeholder="Enter Password">
+            <input type="submit" name="" value="login">
+        </form>
+
+    </div>
+</body>
+</html>
